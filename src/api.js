03f696fae078c81b456f3c98a0fb47761d0c890d
bhavanamb <mbbhavana9@gmail.com>
2022-07-30 20:16:05 -0500

    first commit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoAPIOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "76fa21a64cmshf54db5864a0a5e3p104e19jsn98c78f63c2ef",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
